65b9aa50288e2298bbc47f80b8397d6664b2fcbb
cherubinifranco <cherubini.franco@hotmail.com>
2025-11-23 22:50:29 -0300

    feat: Button can have target: _blank using external = true

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,5 +1,15 @@
-export default function Button({text = "texto", url= "#"}){
-    return (
-        <a href={url} className="py-3 px-6 bg-accent hover:bg-accentHover rounded-lg font-semibold">{text}</a>
-    )
+export default function Button({
+  text = "texto",
+  url = "#",
+  external = false,
+}) {
+  return (
+    <a
+      href={url}
+      target={external ? "_blank" : "_self"}
+      className="py-3 px-6 bg-accent hover:bg-accentHover rounded-lg font-semibold"
+    >
+      {text}
+    </a>
+  );
 }
